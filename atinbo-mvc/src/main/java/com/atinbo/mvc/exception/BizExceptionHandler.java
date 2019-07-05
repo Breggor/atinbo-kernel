7ffc58fe2dc23770e1d07bb2349e26d4431b34cf
Breggor <breggor@163.com>
2019-07-05 10:33:58 +0800

    init

---
+++ atinbo-mvc/src/main/java/com/atinbo/mvc/exception/BizExceptionHandler.java
@@ -0,0 +1,22 @@
+package com.atinbo.mvc.exception;
+
+@RestControllerAdvice
+public class BizExceptionHandler {
+
+    /**
+     * 用于处理通用异常
+     */
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public Result bindException(MethodArgumentNotValidException e) {
+        BindingResult bindingResult = e.getBindingResult();
+
+        String errorMesssage = "校验失败:";
+
+        for (FieldError fieldError : bindingResult.getFieldErrors()) {
+            errorMesssage += fieldError.getDefaultMessage() + ", ";
+        }
+
+        return new Result("00000010", errorMesssage);
+    }
+}
---
+++ atinbo-mvc/src/main/java/com/atinbo/mvc/package-info.java
@@ -0,0 +1,5 @@
+package com.atinbo.mvc;
+
+/**
+ * 针对mvc扩展一下功能
+ */
