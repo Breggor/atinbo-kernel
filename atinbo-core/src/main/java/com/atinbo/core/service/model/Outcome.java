bff62d5ca7e37e0f0c4b311a691757db5a356cb0
Breggor <breggor@163.com>
2019-07-16 22:38:45 +0800

    init

--- atinbo-core/src/main/java/com/atinbo/core/service/model/Outcome.java
+++ atinbo-core/src/main/java/com/atinbo/core/service/model/Outcome.java
@@ -25,7 +25,25 @@ public class Outcome<T extends BaseBO> {
      */
     private String error;
 
+    /**
+     * 失败返回结果
+     *
+     * @param error
+     * @param <E>
+     * @return
+     */
     public static <E extends BaseBO> Outcome<E> ofFail(String error) {
         return new Outcome<E>().setError(error);
+    }
+
+    /**
+     * 成功返回结果
+     *
+     * @param data
+     * @param <E>
+     * @return
+     */
+    public static <E extends BaseBO> Outcome<E> ofSuccess(E data) {
+        return new Outcome<E>().setSuccess(true).setData(data);
     }
 }
