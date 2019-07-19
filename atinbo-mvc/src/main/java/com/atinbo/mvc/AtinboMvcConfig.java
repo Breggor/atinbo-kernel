9d80b2c6b3795332ba6f52785ef783e7a15ba20f
Breggor <breggor@163.com>
2019-07-19 15:05:16 +0800

    init

--- atinbo-mvc/src/main/java/com/atinbo/mvc/AtinboMvcConfig.java
+++ atinbo-mvc/src/main/java/com/atinbo/mvc/AtinboMvcConfig.java
@@ -1,15 +1,12 @@
 package com.atinbo.mvc;
 
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
 
 /**
  * mvc扩展包
+ *
  * @author breggor
  */
-@EnableAutoConfiguration
-@Configuration
 @ComponentScan(basePackageClasses = AtinboMvcConfig.class)
 public class AtinboMvcConfig {
 
