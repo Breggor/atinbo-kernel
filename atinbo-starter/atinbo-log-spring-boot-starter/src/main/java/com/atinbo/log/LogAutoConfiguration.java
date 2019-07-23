d9c8d90953f0020f1e3387be472eee4bf78e5ec1
曾浩 <epdcgsi@dingtalk.com>
2019-07-23 17:44:02 +0800

    日志模块 配置

--- atinbo-starter/atinbo-log-spring-boot-starter/src/main/java/com/atinbo/log/LogAutoConfiguration.java
+++ atinbo-starter/atinbo-log-spring-boot-starter/src/main/java/com/atinbo/log/LogAutoConfiguration.java
@@ -1,7 +1,6 @@
 package com.atinbo.log;
 
 import com.atinbo.log.aspect.SysLogAspect;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableAsync;
@@ -13,9 +12,7 @@ import org.springframework.scheduling.annotation.EnableAsync;
  */
 @EnableAsync
 @Configuration
-@ConditionalOnProperty(prefix = "atinbo.log",name = "enabled", havingValue = "true")
 public class LogAutoConfiguration {
-
 
     @Bean
     public SysLogAspect sysLogAspect() {
