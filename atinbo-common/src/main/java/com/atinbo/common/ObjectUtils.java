125d3e21e438e8324c9f8364413e1c5056597d1f
Breggor <breggor@163.com>
2019-08-29 22:37:38 +0800

    feat: 新增common模块
    工具类：非业务功能

---
+++ atinbo-common/src/main/java/com/atinbo/common/ObjectUtils.java
@@ -0,0 +1,86 @@
+/*
+ * Axelor Business Solutions
+ *
+ * Copyright (C) 2005-2019 Axelor (<http://axelor.com>).
+ *
+ * This program is free software: you can redistribute it and/or  modify
+ * it under the terms of the GNU Affero General Public License, version 3,
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.atinbo.common;
+
+import java.lang.reflect.Array;
+import java.util.Collection;
+import java.util.Map;
+import java.util.Optional;
+
+/** This class defines from static helper methods to deal with objects. */
+public final class ObjectUtils {
+
+  /**
+   * Check whether the given value is an array.
+   *
+   * @param value the value to check
+   * @return true if value is array false otherwise
+   */
+  public static boolean isArray(Object value) {
+    return value != null && value.getClass().isArray();
+  }
+
+  /**
+   * Check whether the given value is empty.
+   *
+   * <p>An object value is empty if:
+   *
+   * <ul>
+   *   <li>value is null
+   *   <li>value is {@link Optional} and {@link Optional#empty()}
+   *   <li>value is {@link Array} with length 0
+   *   <li>value is {@link CharSequence} with length 0
+   *   <li>value is {@link Collection} or {@link Map} with size 0
+   * </ul>
+   *
+   * @param value the object value to check
+   * @return true if empty false otherwise
+   */
+  public static boolean isEmpty(Object value) {
+    if (value == null) {
+      return true;
+    }
+    if (value instanceof Optional) {
+      return !((Optional<?>) value).isPresent();
+    }
+    if (value.getClass().isArray()) {
+      return Array.getLength(value) == 0;
+    }
+    if (value instanceof CharSequence) {
+      return ((CharSequence) value).length() == 0;
+    }
+    if (value instanceof Collection) {
+      return ((Collection<?>) value).size() == 0;
+    }
+    if (value instanceof Map) {
+      return ((Map<?, ?>) value).size() == 0;
+    }
+    return false;
+  }
+
+  /**
+   * Check whether the given value is not empty.
+   *
+   * @param value the object value to check
+   * @return true if not empty false otherwise
+   * @see #isEmpty(Object)
+   */
+  public static boolean notEmpty(Object value) {
+    return !isEmpty(value);
+  }
+}
---
+++ atinbo-common/src/main/java/com/atinbo/common/reflections/ResourceFinder.java
@@ -0,0 +1,125 @@
+/*
+ * Axelor Business Solutions
+ *
+ * Copyright (C) 2005-2019 Axelor (<http://axelor.com>).
+ *
+ * This program is free software: you can redistribute it and/or  modify
+ * it under the terms of the GNU Affero General Public License, version 3,
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.atinbo.common.reflections;
+
+import com.google.common.reflect.ClassPath;
+import com.google.common.reflect.ClassPath.ResourceInfo;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/** The {@link ResourceFinder} class provides fluent api to search for resources. */
+public final class ResourceFinder {
+
+  private ClassLoader loader;
+  private Set<Pattern> namePatterns = new LinkedHashSet<>();
+  private Set<Pattern> pathPatterns = new LinkedHashSet<>();
+
+  ResourceFinder() {}
+
+  ResourceFinder(ClassLoader loader) {
+    this.loader = loader;
+  }
+
+  /**
+   * Find with the given name pattern.
+   *
+   * @param pattern the name pattern
+   * @return the same finder
+   */
+  public ResourceFinder byName(String pattern) {
+    Objects.requireNonNull(pattern, "pattern must not be null");
+    namePatterns.add(Pattern.compile(pattern));
+    return this;
+  }
+
+  /**
+   * Find with the given URL pattern.
+   *
+   * @param pattern the URL pattern
+   * @return the same finder
+   */
+  public ResourceFinder byURL(String pattern) {
+    Objects.requireNonNull(pattern, "pattern must not be null");
+    pathPatterns.add(Pattern.compile(pattern));
+    return this;
+  }
+
+  /**
+   * Search the resources by full pattern match using {@link Matcher#matches()} call.
+   *
+   * @return list of URL objects
+   */
+  public List<URL> match() {
+    return find(false);
+  }
+
+  /**
+   * Search the resources by partial match using {@link Matcher#find()} call.
+   *
+   * @return list of URL objects
+   */
+  public List<URL> find() {
+    return find(true);
+  }
+
+  private List<URL> find(boolean partial) {
+    List<URL> all = new ArrayList<>();
+    for (Pattern namePattern : namePatterns) {
+      for (URL file : getResources(namePattern, loader, partial)) {
+        if (pathPatterns.isEmpty()) {
+          all.add(file);
+          continue;
+        }
+        for (Pattern pathPattern : pathPatterns) {
+          Matcher matcher = pathPattern.matcher(file.getFile());
+          boolean matched = partial ? matcher.find() : matcher.matches();
+          if (matched) {
+            all.add(file);
+          }
+        }
+      }
+    }
+    return Collections.unmodifiableList(all);
+  }
+
+  private static List<URL> getResources(Pattern pattern, ClassLoader loader, boolean partial) {
+    final List<URL> all = new ArrayList<>();
+    final ClassLoader classLoader =
+        loader == null ? Thread.currentThread().getContextClassLoader() : loader;
+    try {
+      for (ResourceInfo info : ClassPath.from(classLoader).getResources()) {
+        String name = info.getResourceName();
+        Matcher matcher = pattern.matcher(name);
+        boolean matched = partial ? matcher.find() : matcher.matches();
+        if (matched) {
+          Enumeration<URL> urls = classLoader.getResources(name);
+          while (urls.hasMoreElements()) {
+            all.add(urls.nextElement());
+          }
+        }
+      }
+    } catch (IOException e) {
+      throw new RuntimeException(e);
+    }
+    return Collections.unmodifiableList(all);
+  }
+}
