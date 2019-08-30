9ab9fcac825b8c03b2ca1670c838f639dee558a2
Breggor <breggor@163.com>
2019-08-30 09:21:27 +0800

    feat: 新增common模块
    工具类：非业务功能

--- atinbo-core/src/main/java/com/atinbo/core/spring/SpringContextHolder.java
+++ atinbo-core/src/main/java/com/atinbo/core/spring/SpringContextHolder.java
@@ -2,13 +2,14 @@ package com.atinbo.core.spring;
 
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.commons.lang3.Validate;
 import org.springframework.beans.factory.DisposableBean;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
 import org.springframework.context.ApplicationEvent;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Component;
+
+import java.util.Objects;
 
 /**
  * 以静态变量保存Spring ApplicationContext, 可在任何代码任何地方任何时候取出ApplicaitonContext.
@@ -90,7 +91,7 @@ public class SpringContextHolder implements ApplicationContextAware, DisposableB
      * 检查ApplicationContext不为空.
      */
     public static void assertApplicationContextInjected() {
-        Validate.validState(applicationContext != null, "applicaitonContext属性未注入, 请在applicationContext.xml中定义SpringContextHolder.");
+        Objects.requireNonNull(applicationContext, "applicaitonContext属性未注入, 请在applicationContext.xml中定义SpringContextHolder.");
     }
 
     /**
