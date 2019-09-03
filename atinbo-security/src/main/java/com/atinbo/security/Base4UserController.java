ddbf063f34d9567b6987ee4304c65cbddb003f46
Breggor <breggor@163.com>
2019-09-03 19:26:26 +0800

    feat: 权限模块

---
+++ atinbo-security/src/main/java/com/atinbo/security/Base4UserController.java
@@ -0,0 +1,37 @@
+package com.atinbo.security;
+
+import com.atinbo.core.exception.HttpAPIException;
+import com.atinbo.security.model.JwtUser;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.Objects;
+
+/**
+ * 能获取当前用户控制类
+ *
+ * @author breggor
+ */
+@Slf4j
+public class Base4UserController {
+
+
+    @Autowired
+    protected HttpServletRequest request;
+
+    @Autowired
+    protected HttpServletResponse response;
+
+
+    protected JwtUser getJwtUser() throws HttpAPIException {
+        JwtUser user = null;
+        if (SecurityContextHolder.getContext().getAuthentication() != null && SecurityContextHolder.getContext().getAuthentication().getPrincipal() != null) {
+            user = (JwtUser) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        }
+        Objects.requireNonNull(user, "当前没有登录用户");
+        return user;
+    }
+}
