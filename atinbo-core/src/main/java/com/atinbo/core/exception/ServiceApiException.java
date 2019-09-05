4f8ff5307c8db1d40d943e2fe357988b8cf52625
Breggor <breggor@163.com>
2019-09-05 11:07:39 +0800

    feat: 权限模块

---
+++ atinbo-core/src/main/java/com/atinbo/core/exception/ServiceApiException.java
@@ -0,0 +1,26 @@
+package com.atinbo.core.exception;
+
+
+/**
+ * service异常
+ *
+ * @author breggor
+ */
+public class ServiceApiException extends Exception {
+
+    public ServiceApiException() {
+        super();
+    }
+
+    public ServiceApiException(String message) {
+        super(message);
+    }
+
+    public ServiceApiException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public ServiceApiException(Throwable cause) {
+        super(cause);
+    }
+}
