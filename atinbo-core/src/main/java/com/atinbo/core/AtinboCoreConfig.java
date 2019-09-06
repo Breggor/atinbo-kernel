52fe9db91775d214c8e2703432a64b4927c78bb5
Breggor <breggor@163.com>
2019-09-06 14:06:22 +0800

    `feat: 权限模块`

---
+++ atinbo-core/src/main/java/com/atinbo/core/AtinboCoreConfig.java
@@ -0,0 +1,13 @@
+package com.atinbo.core;
+
+
+import org.springframework.context.annotation.ComponentScan;
+
+/**
+ * AtinboCore包路径
+ *
+ * @author breggor
+ */
+@ComponentScan(basePackageClasses = AtinboCoreConfig.class)
+public class AtinboCoreConfig {
+}
