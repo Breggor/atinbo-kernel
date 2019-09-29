1010648448626f1fe8e918e79889ac0a19114538
Breggor <breggor@163.com>
2019-09-29 17:00:36 +0800

    `feat: 权限模块`

---
+++ atinbo-security/src/main/java/com/atinbo/security/config/MessageSourceConfig.java
@@ -0,0 +1,23 @@
+package com.atinbo.security.config;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+
+
+/**
+ * message国际化
+ *
+ * @author breggor
+ */
+@Configuration
+public class MessageSourceConfig {
+
+    @Bean
+    public ReloadableResourceBundleMessageSource messageSource() {
+        ReloadableResourceBundleMessageSource source = new ReloadableResourceBundleMessageSource();
+        source.setBasenames("classpath:messages", "classpath:org/springframework/security/messages");
+        return source;
+    }
+}
