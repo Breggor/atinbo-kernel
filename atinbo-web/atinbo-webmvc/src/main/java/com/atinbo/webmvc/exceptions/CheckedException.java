b6122887ab5a3bea4b8f5637dfe4d20169f6ca72
Breggor <breggor@163.com>
2019-10-21 15:30:59 +0800

    `feat: 权限模块`

---
+++ atinbo-web/atinbo-webmvc/src/main/java/com/atinbo/webmvc/exceptions/CheckedException.java
@@ -0,0 +1,45 @@
+/*
+ *  Copyright (c) 2019-2020, 冷冷 (wangiegie@gmail.com).
+ *  <p>
+ *  Licensed under the GNU Lesser General Public License 3.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *  <p>
+ * https://www.gnu.org/licenses/lgpl.html
+ *  <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.atinbo.webmvc.exceptions;
+
+import lombok.NoArgsConstructor;
+
+/**
+ * 检查异常
+ * @author breggor
+ */
+@NoArgsConstructor
+public class CheckedException extends RuntimeException {
+	private static final long serialVersionUID = 1L;
+
+	public CheckedException(String message) {
+		super(message);
+	}
+
+	public CheckedException(Throwable cause) {
+		super(cause);
+	}
+
+	public CheckedException(String message, Throwable cause) {
+		super(message, cause);
+	}
+
+	public CheckedException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+		super(message, cause, enableSuppression, writableStackTrace);
+	}
+
+}
