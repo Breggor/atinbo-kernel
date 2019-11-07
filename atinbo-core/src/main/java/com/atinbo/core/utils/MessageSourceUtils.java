135eb9e973a3af7c14700b9a5c80e2a0e7b17160
Breggor <breggor@163.com>
2019-11-07 12:39:10 +0800

    fix: 权限修改
    权限调整，删除没用代码

--- atinbo-core/src/main/java/com/atinbo/core/utils/MessageSourceUtils.java
+++ atinbo-core/src/main/java/com/atinbo/core/utils/MessageSourceUtils.java
@@ -10,15 +10,40 @@ import org.springframework.context.i18n.LocaleContextHolder;
  * @author breggor
  */
 public class MessageSourceUtils {
+
     /**
      * 根据消息键和参数 获取消息 委托给spring messageSource
      *
      * @param code 消息键
-     * @param args 参数
-     * @return 获取国际化翻译值
+     * @return 获取国际化值
+     */
+    public static String message(String code) {
+        MessageSource messageSource = SpringContextHolder.getBean(MessageSource.class);
+        return messageSource.getMessage(code, null, LocaleContextHolder.getLocale());
+    }
+
+    /**
+     * 根据消息键和参数 获取消息 委托给spring messageSource
+     *
+     * @param code 消息键
+     * @param args 数组参数
+     * @return 获取国际化值
      */
     public static String message(String code, Object... args) {
         MessageSource messageSource = SpringContextHolder.getBean(MessageSource.class);
         return messageSource.getMessage(code, args, LocaleContextHolder.getLocale());
+    }
+
+    /**
+     * 根据消息键和参数 获取消息 委托给spring messageSource
+     *
+     * @param code           消息键
+     * @param args           数组参数
+     * @param defaultMessage 没有设置key的时候的默认值
+     * @return 获取国际化翻译值
+     */
+    public static String message(String code, Object[] args, String defaultMessage) {
+        MessageSource messageSource = SpringContextHolder.getBean(MessageSource.class);
+        return messageSource.getMessage(code, args, defaultMessage, LocaleContextHolder.getLocale());
     }
 }
