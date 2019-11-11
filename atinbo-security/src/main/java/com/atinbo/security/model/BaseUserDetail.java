b1afb317655fe366630e62b4abb5896d52142d3d
Breggor <breggor@163.com>
2019-11-12 00:21:55 +0800

    fix: 结构调整

--- atinbo-security/src/main/java/com/atinbo/security/model/BaseUserDetail.java
+++ atinbo-security/src/main/java/com/atinbo/security/model/BaseUserDetail.java
@@ -1,5 +1,6 @@
 package com.atinbo.security.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -27,6 +28,7 @@ public class BaseUserDetail implements Serializable {
     /**
      * 密码
      */
+    @JsonIgnore
     private String password;
 
     /**
