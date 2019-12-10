8dfcacf72c7477f07c487745cbe9a4e37648bb94
曾浩 <epdcgsi@dingtalk.com>
2019-12-10 09:52:33 +0800

    add:日志工具类调整

--- atinbo-core/src/main/java/com/atinbo/core/utils/DateUtil.java
+++ atinbo-core/src/main/java/com/atinbo/core/utils/DateUtil.java
@@ -429,8 +429,19 @@ public class DateUtil {
      * @param instant Instant
      * @return Instant
      */
-    public static LocalDateTime toDateTime(Instant instant) {
+    public static LocalDateTime toDateTime(final Instant instant) {
         return LocalDateTime.ofInstant(instant, ZoneId.systemDefault());
+    }
+
+
+    /**
+     * Date 转 时间
+     *
+     * @param date Date
+     * @return LocalDateTime
+     */
+    public static LocalDateTime toDateTime(final Date date) {
+        return LocalDateTime.ofInstant(date.toInstant(), ZoneId.systemDefault());
     }
 
     /**
@@ -492,25 +503,6 @@ public class DateUtil {
         return LocalDateTime.ofInstant(calendar.toInstant(), zid);
     }
 
-    /**
-     * 转换成java8 时间
-     *
-     * @param instant Instant
-     * @return LocalDateTime
-     */
-    public static LocalDateTime fromInstant(final Instant instant) {
-        return LocalDateTime.ofInstant(instant, ZoneId.systemDefault());
-    }
-
-    /**
-     * 转换成java8 时间
-     *
-     * @param date Date
-     * @return LocalDateTime
-     */
-    public static LocalDateTime fromDate(final Date date) {
-        return LocalDateTime.ofInstant(date.toInstant(), ZoneId.systemDefault());
-    }
 
     /**
      * 转换成java8 时间
@@ -583,6 +575,47 @@ public class DateUtil {
         } else {
             return StringUtil.format("{}小时{}分{}秒" , hours, minutes, second);
         }
+    }
+
+    /**
+     * 返回指定日期的起始时间
+     *
+     * @param date 指定日期（例如2014-08-01）
+     * @return 返回起始时间（例如2014-08-01 00:00:00）
+     */
+    public static Date getDayStartTime(Date date) {
+        return resetTime(date, 0, 0, 0);
+    }
+
+
+    /**
+     * 返回指定日期的结束时间
+     *
+     * @param date 指定日期（例如2014-08-01）
+     * @return 返回结束时间（例如2014-08-01 23:59:59）
+     */
+    public static Date getDayEndTime(Date date) {
+        return resetTime(date, 23, 59, 59);
+    }
+
+    /**
+     * 获取重置指定日期的时分秒后的时间
+     *
+     * @param date   指定日期
+     * @param hour   指定小时
+     * @param minute 指定分钟
+     * @param second 指定秒
+     * @return 返回重置时分秒后的时间
+     */
+    public static Date resetTime(Date date, int hour, int minute, int second) {
+        Calendar cal = Calendar.getInstance();
+        if (date != null) {
+            cal.setTime(date);
+        }
+        cal.set(Calendar.HOUR_OF_DAY, hour);
+        cal.set(Calendar.SECOND, minute);
+        cal.set(Calendar.MINUTE, second);
+        return cal.getTime();
     }
 
     /**
