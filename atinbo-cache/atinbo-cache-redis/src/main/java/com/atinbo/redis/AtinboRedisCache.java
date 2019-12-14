f8bdcb7d8bf0a8ce60b1faa44440360241dbe3d4
曾浩 <epdcgsi@dingtalk.com>
2019-12-14 10:27:03 +0800

    fix:redis调整

--- atinbo-cache/atinbo-cache-redis/src/main/java/com/atinbo/redis/AtinboRedisCache.java
+++ atinbo-cache/atinbo-cache-redis/src/main/java/com/atinbo/redis/AtinboRedisCache.java
@@ -1,10 +1,6 @@
 package com.atinbo.redis;
 
-import com.atinbo.redis.template.JsonRedisTemplate;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.data.redis.connection.RedisConnectionFactory;
-import org.springframework.data.redis.core.RedisTemplate;
 
 /**
  * 权限配置
@@ -14,8 +10,4 @@ import org.springframework.data.redis.core.RedisTemplate;
 @ComponentScan(basePackageClasses = AtinboRedisCache.class)
 public class AtinboRedisCache {
 
-    @Bean
-    public RedisTemplate redisTemplate(RedisConnectionFactory connectionFactory){
-        return new JsonRedisTemplate(connectionFactory);
-    }
 }
