b51157d70d5aaa56dffa6c32b6b17cfb376ba70f
Breggor <breggor@163.com>
2019-12-20 10:13:57 +0800

    恢复

--- atinbo-security/src/main/java/com/atinbo/security/service/LoginService.java
+++ atinbo-security/src/main/java/com/atinbo/security/service/LoginService.java
@@ -17,6 +17,7 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.Resource;
@@ -73,6 +74,8 @@ public class LoginService {
         try {
             // 该方法会去调用UserDetailsServiceImpl.loadUserByUsername
             authentication = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(username, password));
+        } catch (UsernameNotFoundException ex) {
+            throw ex;
         } catch (Exception e) {
             if (e instanceof BadCredentialsException) {
                 addFailedCount(username);
