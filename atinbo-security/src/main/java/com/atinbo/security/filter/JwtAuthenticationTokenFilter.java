7c597049323e661de936903cf1077d06bec7b698
Breggor <breggor@163.com>
2019-12-21 13:54:07 +0800

    框架扩展模块

--- atinbo-security/src/main/java/com/atinbo/security/filter/JwtAuthenticationTokenFilter.java
+++ atinbo-security/src/main/java/com/atinbo/security/filter/JwtAuthenticationTokenFilter.java
@@ -5,11 +5,13 @@ import com.atinbo.security.model.LoginUser;
 import com.atinbo.security.service.UserTokenService;
 import com.atinbo.security.utils.SecurityUtils;
 import lombok.extern.slf4j.Slf4j;
+import org.slf4j.MDC;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import javax.servlet.FilterChain;
@@ -17,6 +19,7 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.text.MessageFormat;
 import java.util.Objects;
 
 /**
@@ -31,9 +34,33 @@ public class JwtAuthenticationTokenFilter extends OncePerRequestFilter {
     @Autowired
     private UserTokenService userTokenService;
 
+
+    private static final String REQUEST_ID = "requestId";
+    ThreadLocal<Long> actTime = new ThreadLocal<>();
+
+
+    public void before(HttpServletRequest request) {
+        actTime.set(System.currentTimeMillis());
+        String requestId = request.getHeader(REQUEST_ID);
+        log.info("Header: requestId({})", requestId);
+
+        if (!StringUtils.isEmpty(requestId)) {
+            requestId = request.getParameter(REQUEST_ID);
+            log.info("Body: requestId({})", requestId);
+        }
+
+        if (StringUtils.isEmpty(requestId)) {
+            requestId = System.currentTimeMillis() + "";
+        }
+
+        MDC.put(REQUEST_ID, requestId);
+        log.info(MessageFormat.format("url={0}, method={0}, queryString={0}", request.getRequestURL().toString(), request.getMethod().toLowerCase(), request.getQueryString()));
+    }
+
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain chain) throws ServletException, IOException {
         log.info("req path=[{}]", request.getRequestURI());
+        before(request);
 
         if (!request.getRequestURI().contains(LOGIN_PATH)) {
             LoginUser loginUser = userTokenService.getLoginUser(request);
@@ -45,5 +72,14 @@ public class JwtAuthenticationTokenFilter extends OncePerRequestFilter {
             }
         }
         chain.doFilter(request, response);
+        after(request);
+    }
+
+
+    public void after(HttpServletRequest request) {
+        long consumeTime = System.currentTimeMillis() - actTime.get();
+        log.info("[" + request.getRequestURI() + "] consume time:" + consumeTime);
+        MDC.clear();
+        actTime.remove();
     }
 }
