25c440ae1747983cd54664bc29589585d9db3f7f
Breggor <breggor@163.com>
2019-12-21 13:06:14 +0800

    框架扩展模块

--- atinbo-pressure/dubbo-filter-mdc/src/main/java/com/atinbo/dubbo/mdc/MdcLoggorFilter.java
+++ atinbo-pressure/dubbo-filter-mdc/src/main/java/com/atinbo/dubbo/mdc/MdcLoggorFilter.java
@@ -9,6 +9,9 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.slf4j.MDC;
 
+import java.util.HashMap;
+import java.util.Map;
+
 
 /**
  * MDCFilter
@@ -23,7 +26,8 @@ public class MdcLoggorFilter implements Filter {
     @Override
     public Result invoke(Invoker<?> invoker, Invocation invocation) throws RpcException {
         try {
-            String param = "";
+            Map<String, Object> paramMap = new HashMap();
+
             if (RpcContext.getContext().isConsumerSide()) {
                 if (StringUtils.isBlank(MDC.get(REQ_ID))) {
                     logger.warn("slf4j MDC [requestId] don't setting!");
@@ -35,14 +39,17 @@ public class MdcLoggorFilter implements Filter {
                 if (StringUtils.isNotEmpty(requestId)) {
                     MDC.put(REQ_ID, requestId);
                 }
+
                 //dubbo请求参数
-                Object[] args = RpcContext.getContext().getArguments();
-                if (args != null && args.length > 0) {
-                    param = JSON.toJSONString(args);
+                Class[] paramTypes = RpcContext.getContext().getParameterTypes();
+                if (paramTypes != null && paramTypes.length > 0) {
+                    for (int i = 0, len = paramTypes.length; i < len; i++) {
+                        paramMap.put(paramTypes[i].getSimpleName(), RpcContext.getContext().getArguments()[i]);
+                    }
                 }
             }
 
-            logger.info("Dubbo MDC Filter - Interface=[{}], Method=[{}], Params=[{}]", invoker.getInterface().getName(), RpcContext.getContext().getMethodName(), param);
+            logger.info("Dubbo MdcFilter Interface={}, Method={}, Params={}", invoker.getInterface().getName(), RpcContext.getContext().getMethodName(), JSON.toJSONString(paramMap));
         } catch (Exception e) {
             logger.warn("Exception in MDCFilter of service(" + invoker + " -> " + invocation + ")", e);
             throw new RpcException(e.getMessage(), e);
