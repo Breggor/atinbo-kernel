b1eb96202f6a41e9424218506445e9d7fd59a0c4
Breggor <breggor@163.com>
2019-12-21 15:34:46 +0800

    框架扩展模块

--- atinbo-web/atinbo-webmvc/src/main/java/com/atinbo/webmvc/filter/RequestIdFilter.java
+++ atinbo-web/atinbo-webmvc/src/main/java/com/atinbo/webmvc/filter/RequestIdFilter.java
@@ -23,38 +23,36 @@ import java.text.MessageFormat;
 public class RequestIdFilter extends OncePerRequestFilter {
     private static final String REQUEST_ID = "requestId";
 
-    ThreadLocal<Long> actTime = new ThreadLocal<>();
+    private final ThreadLocal<Long> actTime = new InheritableThreadLocal<>();
 
     private void before(HttpServletRequest request) {
         actTime.set(System.currentTimeMillis());
         String requestId = request.getHeader(REQUEST_ID);
         log.info("Header: requestId({})", requestId);
-
         if (!StringUtils.isEmpty(requestId)) {
             requestId = request.getParameter(REQUEST_ID);
             log.info("Body: requestId({})", requestId);
         }
-
         if (StringUtils.isEmpty(requestId)) {
             requestId = System.currentTimeMillis() + "";
         }
-
         MDC.put(REQUEST_ID, requestId);
-        log.info(MessageFormat.format("url={0}, method={0}, queryString={0}", request.getRequestURL().toString(), request.getMethod().toLowerCase(), request.getQueryString()));
+        log.info(MessageFormat.format("request path={0}", request.getRequestURI()));
     }
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain chain) throws ServletException, IOException {
-        log.info("request path=[{}]", request.getRequestURI());
         before(request);
+
         chain.doFilter(request, response);
+
         after(request);
     }
 
 
     private void after(HttpServletRequest request) {
         long consumeTime = System.currentTimeMillis() - actTime.get();
-        log.info("request path={}, Elapsed time:{}", request.getRequestURI(), consumeTime);
+        log.info("request path={}, Elapsed time:{} (ms)", request.getRequestURI(), consumeTime);
         MDC.clear();
         actTime.remove();
     }
